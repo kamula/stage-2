1b39130bec8a90ffecdbd027afede36e9521ceaf
Isaac Kamula <kamulaisaac15@gmail.com>
2021-08-17 21:57:26 +0300

    resume app model update

--- Backend Track/resume/models.py
+++ Backend Track/resume/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 import uuid
 
 class Message(models.Model):
+    '''message model'''
     id = models.UUIDField(primary_key = True,default = uuid.uuid4,editable = False)
     email = models.EmailField(max_length=200)
     message = models.TextField()
